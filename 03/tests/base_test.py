ed9b901eb225783a7d24aeefaf8cca24a129b6e2
Canyon <jorislisas@gmail.com>
2025-03-04 18:18:34 +0200

    options 2

--- 03/tests/base_test.py
+++ 03/tests/base_test.py
@@ -1,7 +1,9 @@
 import unittest
+import tempfile
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from utils.user_creator import create_user
+from selenium.webdriver.chrome.options import Options
 
 class BaseTest(unittest.TestCase):
     @classmethod
@@ -9,7 +11,12 @@ class BaseTest(unittest.TestCase):
         cls.email, cls.password = create_user()
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        temp_user_data_dir = tempfile.mkdtemp()
+        
+        chrome_options = Options()
+        chrome_options.add_argument(f"--user-data-dir={temp_user_data_dir}")
+        
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get("https://demowebshop.tricentis.com/")
         self.wait = WebDriverWait(self.driver, 5)
 
